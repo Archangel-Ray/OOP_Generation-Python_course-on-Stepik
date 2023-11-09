7a68330cd0dc6eeb45a2d6ccda99b90f86996813
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2023-11-09 14:48:08 +0200

    4.1 Атрибуты объектов и классов
    ну и ситуацию я себе создал.
    один курс повис не законченным, а то что не пройдено уже нужно знать.
    по этому постоянно возвращаюсь к нему.
    потом, начал курс на СкиллФактори, в котором постоянные дедлайны и
    надо его постоянно вести.
    и этот, третий, который уже надо закончить.
    плюс ко всему этому работу никто не отменял.
    ох... и не знаю как долго я в таком положении выдежу.

---
+++ 4. Атрибуты, свойства и методы/4.1 Атрибуты объектов и классов/01 в первый раз в первый класс.py
@@ -0,0 +1,9 @@
+# Реализуйте пустой класс PiggyBank, а затем создайте экземпляр этого класса и присвойте его переменной money_box.
+
+
+class PiggyBank:
+    pass
+
+
+money_box = PiggyBank()
+print(money_box.__dir__())
